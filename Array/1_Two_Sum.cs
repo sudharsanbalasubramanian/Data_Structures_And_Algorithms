9267bed9ea841adc5a05dfb8f3d9cfcd7069d5f8
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2026-01-01 18:53:10 +0530

    Add solutions for Two Sum and Longest Common Prefix
    Implemented _1_Two_Sum and _14_Longest_Common_Prefix classes with efficient solutions for their respective problems. Added an empty _49_Group_Anagrams class as a placeholder for future implementation. Included appropriate namespaces and using directives.

---
+++ Array/1_Two_Sum.cs
@@ -0,0 +1,27 @@
+﻿namespace Data_Structures_And_Algorithms.Array;
+//https://leetcode.com/problems/two-sum/
+public sealed class _1_Two_Sum
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> dict = [];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (dict.TryGetValue(complement, out int index))
+            {
+                return [index, i];
+            }
+
+            if (!dict.ContainsKey(nums[i]))
+            {
+                dict[nums[i]] = i;
+            }
+        }
+
+        return [-1, -1];
+    }
+
+}
---
+++ Array/49_Group_Anagrams.cs
@@ -0,0 +1,5 @@
+﻿namespace Data_Structures_And_Algorithms.Array;
+
+public sealed class _49_Group_Anagrams
+{
+}
