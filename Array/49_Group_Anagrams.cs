9f0458cb7a5a0134432a50ae40681bd06a43b763
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2026-01-02 15:08:53 +0530

    Add GroupAnagrams method to group strings by anagrams
    Implemented GroupAnagrams as a static method in the _49_Group_Anagrams class. This method groups input strings into lists of anagrams by using character frequency counts as keys in a dictionary, then returns the grouped results.

--- Array/49_Group_Anagrams.cs
+++ Array/49_Group_Anagrams.cs
@@ -2,4 +2,30 @@
 
 public sealed class _49_Group_Anagrams
 {
+    public static IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        var dict = new Dictionary<string, IList<string>>();
+
+        foreach (var str in strs)
+        {
+            int[] counts = new int[26];
+
+            foreach (char c in str)
+            {
+                counts[c - 'a']++;
+            }
+
+            string key = string.Join('#', counts);
+
+            if (!dict.TryGetValue(key, out var list))
+            {
+                dict[key] = list = [];
+            }
+
+            list.Add(str);
+        }
+
+        return [.. dict.Values];
+    }
+
 }
